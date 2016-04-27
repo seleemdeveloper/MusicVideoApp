ac43334651e4047e9399000c008b9fad9e585614
Ahmed Seleem <seleem.developer@gmail.com>
2016-04-27 08:12:16 +0300

    Completed Part-2 Code session

---
+++ MusicVideoApp/APIManager.swift
@@ -0,0 +1,43 @@
+//
+//  APIManager.swift
+//  MusicVideoApp
+//
+//  Created by ahmed seleem on 4/26/16.
+//  Copyright © 2016 ahmed seleem. All rights reserved.
+//
+
+import Foundation
+
+class APIManager
+{
+   
+    
+    func loadData(urlString: String,completion: (result:String) -> Void )
+    {
+     
+        let sessionConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        let session = NSURLSession(configuration: sessionConfig)
+        
+        let url = NSURL(string: urlString)!
+        let task = session.dataTaskWithURL(url)
+        {
+         (data, response, error) in
+         
+            dispatch_async(dispatch_get_main_queue())
+            {
+             if error != nil
+             {
+              completion(result: error!.localizedDescription)
+             }
+             else
+             {
+              completion(result: "NSURLSession succefull")
+              print(data!)
+             }
+            }
+        }
+        task.resume()
+    }
+    
+    
+}
